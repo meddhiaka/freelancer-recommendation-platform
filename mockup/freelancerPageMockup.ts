2adc0f9fdf0fbce0295b34aa3074e413eb507435
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-13 01:38:33 +0100

    feat: added freelancer page mockup fle

---
+++ mockup/freelancerPageMockup.ts
@@ -0,0 +1,46 @@
+const freelancerCards : Array<Mockup.ICard> = [
+    {
+        id: "1",
+        firstName: "Aymen",
+        lastName: "Ben Ali",
+        role: "Software Engineer",
+        profilePicture: "profilePictures/m1.jpg"
+    },
+    {
+        id: "2",
+        firstName: "Fatma",
+        lastName: "Trabelsi",
+        role: "Data Scientist",
+        profilePicture: "profilePictures/f1.jpg"
+    },
+    {
+        id: "3",
+        firstName: "Mohamed",
+        lastName: "Hammami",
+        role: "Machine Learning Engineer",
+        profilePicture: "profilePictures/m2.jpg"
+    },
+    {
+        id: "4",
+        firstName: "Salma",
+        lastName: "Khaled",
+        role: "AI Researcher",
+        profilePicture: "profilePictures/f2.jpg"
+    },
+    {
+        id: "5",
+        firstName: "Nour",
+        lastName: "Maaloul",
+        role: "DevOps Engineer",
+        profilePicture: "profilePictures/f3.jpg"
+    },
+    {
+        id: "6",
+        firstName: "Rania",
+        lastName: "Bouzid",
+        role: "Frontend Developer",
+        profilePicture: "profilePictures/f4.jpg"
+    }
+]
+
+export default freelancerCards
