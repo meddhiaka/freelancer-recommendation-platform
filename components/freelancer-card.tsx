891a519a6c75b73f3e853386eb34bd3209785e79
Mohamed Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-17 14:27:05 +0100

    feat: added custom freelancer card component

--- components/freelancer-card.tsx
+++ components/freelancer-card.tsx
@@ -5,25 +5,45 @@ import {
     CardFooter,
     CardHeader,
 } from "@/components/ui/card"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import { Badge } from "./ui/badge"
 
-export function FreelancerCard() {
+export function FreelancerCard({freelancer}: {freelancer: Mockup.ICard}) {
     return (
-        <Card className="w-[400px] rounded-none">
-            <CardHeader>
-                card header
+        <Card className="w-[400px] rounded-none space-y-1">
+            <CardHeader className="pt-2 pb-3 px-3 flex flex-row justify-between">
+                <Image
+                    src={freelancer.profilePicture}
+                    width={80}
+                    height={80}
+                    alt="Picture of the freelancer"
+                    className="rounded-full"
+                />
+                <Button
+                    variant="secondary"
+                    className="font-bold text-lg rounded-none text-primary"
+                >
+                    Voir
+                </Button>
             </CardHeader>
-            <CardContent>
+            <CardContent className="py-1 px-3">
                 <form>
                     <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            card content
+                        <div className="flex flex-col font-bold text-2xl">
+                            {freelancer.firstName}
+                        </div>
+                        <div className="flex flex-col text-base">
+                            {freelancer.description}
                         </div>
                     </div>
                 </form>
             </CardContent>
-            <CardFooter className="flex justify-between">
+            <CardFooter className="pt-3 px-3 pb-4 flex justify-between">
                 <div>
-                    card footer
+                    <Badge className="text-primary border-primary text-sm py-1 rounded-lg" variant="outline">
+                        {freelancer.role}
+                    </Badge>
                 </div>
             </CardFooter>
         </Card>
