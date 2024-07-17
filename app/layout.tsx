0525546057e6355771e5871e79002ad28e27b578
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-17 12:29:17 +0100

    feat: setup the navigation bar component in the home layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
+import NavigationBar from "@/components/nav-bar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,6 +26,9 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}>
+        <div className="border-b-[2.5px] shadow-lg shadow-gray-50">
+          <NavigationBar />
+        </div>
         {children}
       </body>
     </html>
