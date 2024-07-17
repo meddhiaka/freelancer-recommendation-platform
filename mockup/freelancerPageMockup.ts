2ca5496072c45d61716eb5f51ea1263ce18903fb
Mohamed Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-17 14:27:48 +0100

    fix: added missing description data to freelancer cards mockup

--- mockup/freelancerPageMockup.ts
+++ mockup/freelancerPageMockup.ts
@@ -4,42 +4,48 @@ const freelancerCards : Array<Mockup.ICard> = [
         firstName: "Aymen",
         lastName: "Ben Ali",
         role: "Software Engineer",
-        profilePicture: "profilePictures/m1.jpg"
+        profilePicture: "/profilePictures/m1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     },
     {
         id: "2",
         firstName: "Fatma",
         lastName: "Trabelsi",
         role: "Data Scientist",
-        profilePicture: "profilePictures/f1.jpg"
+        profilePicture: "/profilePictures/f1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     },
     {
         id: "3",
         firstName: "Mohamed",
         lastName: "Hammami",
         role: "Machine Learning Engineer",
-        profilePicture: "profilePictures/m2.jpg"
+        profilePicture: "/profilePictures/m2.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     },
     {
         id: "4",
         firstName: "Salma",
         lastName: "Khaled",
         role: "AI Researcher",
-        profilePicture: "profilePictures/f2.jpg"
+        profilePicture: "/profilePictures/f2.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     },
     {
         id: "5",
         firstName: "Nour",
         lastName: "Maaloul",
         role: "DevOps Engineer",
-        profilePicture: "profilePictures/f3.jpg"
+        profilePicture: "/profilePictures/f3.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     },
     {
         id: "6",
         firstName: "Rania",
         lastName: "Bouzid",
         role: "Frontend Developer",
-        profilePicture: "profilePictures/f4.jpg"
+        profilePicture: "/profilePictures/f4.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident, quis error eius eveniet necessitatibus consequuntur rerum vitae! Reprehenderit, incidunt."
     }
 ]
 
