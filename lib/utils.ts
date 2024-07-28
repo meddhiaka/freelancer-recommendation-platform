ea8583d98c6716851cf588f28d1c9bf3a8ebd1b0
meddhiaka <mohameddhiakb@gmail.com>
2024-07-28 12:40:20 +0100

    feat: added asynchronous function with a promise to find the freelancer object

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function findFreelancerProfile(L: Array<Mockup.IFreelancerProfile>, x: string): Promise<Mockup.IFreelancerProfile | null> {
+  return new Promise((resolve, reject) => {
+      const existingProfile = L.find((e) => e.id === x);
+      if (existingProfile) {
+          resolve(existingProfile);
+      } else {
+          reject(null);
+      }
+  })
+}
